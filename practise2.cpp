5713624fac1fca8944c46afd08d58d297acde7d5
junaid11hussain22@female.com <junaid11hussain22@gmail.com>
2025-01-30 12:29:50 +0530

    add dfs algorithm

--- practise2.cpp
+++ practise2.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void bfs(int s, unordered_map<int,bool>&visited,unordered_map<int,list<int>>adjList){
     queue<int>q;
     q.push(s);
+    visited[s]=true;
     while(!q.empty()){
         int fNode = q.front();
         cout<<fNode<<" ";
@@ -20,13 +21,34 @@ void bfs(int s, unordered_map<int,bool>&visited,unordered_map<int,list<int>>adjL
     }
 
 }
+
+//---------------------------------DFS----------------------------
+void dfs(int s,unordered_map<int,bool>&vis,unordered_map<int,list<int>>adjList){
+    cout<<s<<" ";
+    vis[s]=true;
+    for(auto it: adjList[s]){
+        if(!vis[it]){
+            dfs(it,vis,adjList);
+        }
+    }
+}
 int main(){
+//-------------BFS-----------------------
+    // unordered_map<int,list<int>>adjList;
+    // unordered_map<int,bool>visited(false);
+    // int n = 4;
+    // for(int i=0;i<=n;i++){
+    //     if(!visited[i]){
+    //         bfs(i,visited,adjList);
+    //     }
+    // }
+//------------dfs-----------------------
     unordered_map<int,list<int>>adjList;
     unordered_map<int,bool>visited(false);
     int n = 4;
     for(int i=0;i<=n;i++){
         if(!visited[i]){
-            bfs(i,visited,adjList);
+            dfs(i,visited,adjList);
         }
     }
      
